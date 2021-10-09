00b0e5a133ea7034813eb2727335fa58520bf872
Silas <silas.johnson86@gmail.com>
2021-10-09 14:27:08 -0500

    redux adding favorites

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,19 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+}
 
 
 function Mission() {
-    return(
+    return (
         <Card
             title="Our Mission">
 
-            <Text style={{margin: 10}}>
-            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            <Text style={{ margin: 10 }}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
     )
@@ -21,14 +29,7 @@ function Mission() {
 
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners : PARTNERS
-        };
 
-
-    };
 
     static navigationOptions = {
         title: 'About Us'
@@ -38,35 +39,56 @@ class About extends Component {
 
 
     render() {
-        const renderPartner = ({item}) => {
+        const renderPartner = ({ item }) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
                 />
             );
         };
 
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+
+            )
+        }
+        if (this.props.partners.errMess) {
+            <ScrollView>
+                <Mission />
+                <Card title='Community Partners'>
+                    <Text>{this.props.partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+
+        }
         return (
             <ScrollView>
-                 <Mission />  
-                 <Card title='Community Partners'>
-                 <FlatList
-                data={this.state.partners}
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
-            />
+                <Mission />
+                <Card title='Community Partners'>
+                    <FlatList
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
 
-                 </Card>
+                </Card>
             </ScrollView>
 
         );
-        
+
     }
-    
+
 }
 
 
-export default About;
+export default connect(mapStateToProps)(About);
